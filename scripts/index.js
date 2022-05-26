aaac00a25ff8894fb88c926de0607a284094138b
Yalon <alenad27.12@gmail.com>
2022-05-26 18:05:50 +0500

    Fix: вторые правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 let form = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let popupText = document.querySelector('.popup__input_text');
-let popupActivity = document.querySelector('.popup__input_activity');
+let popupText = document.querySelector('.popup__input_field_text');
+let popupActivity = document.querySelector('.popup__input_field_activity');
 
 function popupOpenedAdd() {
     form.classList.add('popup_opened');
-     popupText.value = document.querySelector('.profile__name').textContent;
-     popupActivity.value = document.querySelector('.profile__activity').textContent;
-    }
+    popupText.value = document.querySelector('.profile__name').textContent;
+    popupActivity.value = document.querySelector('.profile__activity').textContent;
+}
 editButton.addEventListener('click', popupOpenedAdd);
  
+let closeButton = document.querySelector('.popup__close-button');
 function popupOpenedRemove() {
     form.classList.remove('popup_opened');  
-    }
-let closeButton = document.querySelector('.popup__close-button');
+}
 closeButton.addEventListener('click', popupOpenedRemove);
 
+let FormProfile = document.querySelector('.popup__form_type_profile');
 function formSubmitHandler(evt) {
-evt.preventDefault();
-let profileName = document.querySelector('.profile__name');
-let profileActivity = document.querySelector('.profile__activity');
-profileName.textContent = popupText.value;
-profileActivity.textContent = popupActivity.value;
-};
-let FormProfile = document.querySelector('.popup__form_profile');
+    evt.preventDefault();
+    let profileName = document.querySelector('.profile__name');
+    let profileActivity = document.querySelector('.profile__activity');
+    profileName.textContent = popupText.value;
+    profileActivity.textContent = popupActivity.value;
+    popupOpenedRemove();
+}
 FormProfile.addEventListener('submit', formSubmitHandler);
-FormProfile.addEventListener('submit', popupOpenedRemove);
 
