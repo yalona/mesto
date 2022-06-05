4defb2ee90cd7b89c815bdf87a9a14a0dbee19bf
Yalon <alenad27.12@gmail.com>
2022-06-05 21:18:14 +0500

    feat: форма для добавления картинок

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,31 @@
-let form = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
+let popupProfile = document.querySelector('.popup_type_profile');
+let popupNewPic = document.querySelector('.popup_type_new-pic');
 let popupText = document.querySelector('.popup__input_field_text');
 let popupActivity = document.querySelector('.popup__input_field_activity');
 
-function popupOpenedAdd() {
-    form.classList.add('popup_opened');
+// функция открытия попапа
+function popupOpenedAdd(element) {
+    element.classList.add('popup_opened');
     popupText.value = document.querySelector('.profile__name').textContent;
     popupActivity.value = document.querySelector('.profile__activity').textContent;
 }
-editButton.addEventListener('click', popupOpenedAdd);
+// открытие формы с параметрами профиля
+let editButton = document.querySelector('.profile__edit-button');
+editButton.addEventListener('click', () => popupOpenedAdd(popupProfile));
  
-let closeButton = document.querySelector('.popup__close-button');
-function popupOpenedRemove() {
-    form.classList.remove('popup_opened');  
+// Удаление класса из попапа для закрытия
+function popupOpenedRemove(element) {
+    element.classList.remove('popup_opened');  
 }
-closeButton.addEventListener('click', popupOpenedRemove);
 
+// Закрытие попапа
+let closeButton = document.querySelectorAll('.popup__close-button');
+closeButton.forEach(function (btn) {
+    btn.addEventListener('click', () => {
+    popupOpenedRemove(popupProfile);
+})
+ });
+// функция сохранения параметров из формы в профиль
 let FormProfile = document.querySelector('.popup__form_type_profile');
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -23,7 +33,17 @@ function formSubmitHandler(evt) {
     let profileActivity = document.querySelector('.profile__activity');
     profileName.textContent = popupText.value;
     profileActivity.textContent = popupActivity.value;
-    popupOpenedRemove();
+    popupOpenedRemove(popupProfile);
 }
 FormProfile.addEventListener('submit', formSubmitHandler);
 
+// открытие формы добавления картинок
+const addButton = document.querySelector('.profile__add-button');
+addButton.addEventListener('click', () => popupOpenedAdd(popupNewPic));
+
+// Закрытие попапа
+closeButton.forEach(function (btn) {
+    btn.addEventListener('click', () => {
+    popupOpenedRemove(popupNewPic);
+})
+ });
